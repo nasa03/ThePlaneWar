ff3fcedd854d877c3c18a121518a2633b486aa77
SmallPig <664235822@qq.com>
2018-12-02 13:28:59 +0800

    修改场景并初步添加Network功能

---
+++ Assets/Script/MyNetworkManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyNetworkManager : NetworkManager {
+    [SerializeField] Mesh[] playerMesh;
+
+    public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
+    {
+        GameObject player = Instantiate(playerPrefab, Vector3.zero, Quaternion.identity);
+        //设置player的一些参数
+        player.GetComponent<MeshFilter>().mesh = playerMesh[playerControllerId];
+        MeshRenderer renderer = player.GetComponent<MeshRenderer>();
+        BoxCollider collider = player.GetComponent<BoxCollider>();
+        collider.center = renderer.bounds.center;
+        collider.size = renderer.bounds.size;
+
+        NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
+    }
+}
