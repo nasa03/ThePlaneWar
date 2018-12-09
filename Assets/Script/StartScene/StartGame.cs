ebd982b963217eb0a83182e3c12acf7f3307f7d2
SmallPig <664235822@qq.com>
2018-12-09 15:49:03 +0800

    添加自定义的开始游戏UI

---
+++ Assets/Script/StartScene/StartGame.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class StartGame : MonoBehaviour {
+    [SerializeField] NetworkManager networkManager;
+    [SerializeField] UIPopupList popupList;
+
+    public void OnClick()
+    {
+        switch (popupList.GetComponentInChildren<UILabel>().text)
+        {
+            case "作为主机":
+                networkManager.StartHost();
+                break;
+            case "作为客户端":
+                networkManager.StartClient();
+                break;
+            case "作为服务器":
+                networkManager.StartServer();
+                break;
+        }
+    }
+}
