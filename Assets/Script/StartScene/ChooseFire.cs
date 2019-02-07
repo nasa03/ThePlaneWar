c0302864c8e73c6eba1f1bc2d023e1a2ed6dbdd9
SmallPig <664235822@qq.com>
2019-02-07 13:53:36 +0800

    修复切换子弹时的错误

--- Assets/Script/StartScene/ChooseFire.cs
+++ Assets/Script/StartScene/ChooseFire.cs
@@ -6,11 +6,36 @@ public class ChooseFire : SingletonManager<ChooseFire> {
     [SerializeField] UIPopupList fireList;
     [SerializeField] UIPopupList sizeList;
     [HideInInspector] public int index = 0;
+    int fireInt = 0;
+    int sizeInt = 0;
 
-    public void Choose()
+    public void Fire()
     {
-        int fireInt = GetItemsInt(fireList);
-        int sizeInt = GetItemsInt(sizeList);
+        fireInt = GetItemsInt(fireList);
+
+        GetIndex();
+    }
+
+    public void Size()
+    {
+        sizeInt = GetItemsInt(sizeList);
+
+        GetIndex();
+    }
+
+    int GetItemsInt(UIPopupList list)
+    {
+        for (int i = 0; i < list.items.Count; i++)
+        {
+            if (list.value == list.items[i])
+                return i;
+        }
+
+        return 0;
+    }
+
+    void GetIndex()
+    {
         if (fireInt < 2)
         {
             if (sizeList.items.Count == 3)
@@ -25,16 +50,5 @@ public class ChooseFire : SingletonManager<ChooseFire> {
 
             index = 8 + (fireInt - 2) * 3 + sizeInt;
         }
-    }
-
-    int GetItemsInt(UIPopupList list)
-    {
-        for (int i = 0; i < list.items.Count; i++)
-        {
-            if (list.value == list.items[i])
-                return i;
-        }
-
-        return 0;
     }
 }
