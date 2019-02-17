5d4bd6d6aca50a91c0b6127988123a0942fa05e1
SmallPig <664235822@qq.com>
2019-02-17 20:23:00 +0800

    添加游戏网络服务器代码

--- Assets/Script/StartScene/StartGame.cs
+++ Assets/Script/StartScene/StartGame.cs
@@ -1,25 +1,119 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.Networking.Match;
+using System.Net;
+using System.Net.Sockets;
 
 public class StartGame : MonoBehaviour {
     [SerializeField] NetworkManager networkManager;
-    [SerializeField] UIPopupList popupList;
+    [SerializeField] UIPopupList networkList;
+    [SerializeField] UIPopupList serverList;
+    string ipAddress = "";
+    List<MatchInfoSnapshot> serverMatchList = null;
 
-    public void OnClick()
+    // Start is called before the first frame update
+    void Start()
     {
-        switch (popupList.GetComponentInChildren<UILabel>().text)
+        networkManager.StartMatchMaker();
+
+        IPAddress[] ips = Dns.GetHostAddresses(Dns.GetHostName());
+        foreach (IPAddress ip in ips)
+        {
+            if (ip.AddressFamily == AddressFamily.InterNetwork)
+            {
+                ipAddress = ip.ToString();
+            }
+        }
+    }
+
+    public void OnNetworkListValueChanged()
+    {
+        if (networkList.value == "作为客户端")
+        {
+            serverList.gameObject.SetActive(true);
+            networkManager.matchMaker.ListMatches(0, 20, "", true, 0, 0, OnMatchesList);
+        }
+        else
+            serverList.gameObject.SetActive(false);
+    }
+
+    public void OnStartButtonClick()
+    {
+        switch (networkList.GetComponentInChildren<UILabel>().text)
         {
             case "作为主机":
-                networkManager.StartHost();
+                networkManager.matchMaker.CreateMatch(ipAddress, 4, true, "", "", "", 0, 0, OnMatchCreateHost);
                 break;
             case "作为客户端":
-                networkManager.StartClient();
+                int index = 0;
+                for(int i=0; i<serverList.items.Count;i++)
+                {
+                    if(serverList.value==serverList.items[i])
+                    {
+                        index = i;
+                        break;
+                    }
+                }
+                networkManager.matchMaker.JoinMatch(serverMatchList[index].networkId, "", "", "", 0, 0, OnMatchJoined);
                 break;
             case "作为服务器":
-                networkManager.StartServer();
+                networkManager.matchMaker.CreateMatch(ipAddress, 4, true, "", "", "", 0, 0, OnMatchCreateServer);
                 break;
         }
+    }
+
+    private void OnMatchesList(bool success, string extendedInfo, List<MatchInfoSnapshot> responseData)
+    {
+        if (!success)
+        {
+            Debug.LogWarning("List Matches Failed!");
+            return;
+        }
+
+        serverList.items.Clear();
+
+        foreach (MatchInfoSnapshot info in responseData)
+        {
+            serverList.AddItem(info.name);
+        }
+
+        serverMatchList = responseData;
+    }
+
+    private void OnMatchCreateHost(bool success, string extendedInfo, MatchInfo responseData)
+    {
+        if (!success)
+        {
+            Debug.LogWarning("Create Match Failed!");
+            return;
+        }
+
+        networkManager.StartHost(responseData);
+    }
+
+    private void OnMatchJoined(bool success, string extendedInfo, MatchInfo responseData)
+    {
+        if (!success)
+        {
+            Debug.LogWarning("Join Match Failed!");
+            return;
+        }
+
+        networkManager.StartClient(responseData);
+    }
+
+
+    private void OnMatchCreateServer(bool success, string extendedInfo, MatchInfo responseData)
+    {
+        if (!success)
+        {
+            Debug.LogWarning("Create Match Failed!");
+            return;
+        }
+        
+        networkManager.StartServer(responseData);
     }
 }
