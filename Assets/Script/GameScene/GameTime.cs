ddd999fc1ed2c2ae892c3ab3bbcf0d8f404a9c1f
SmallPig <664235822@qq.com>
2019-09-23 21:19:25 +0800

    添加游戏剩余时间标签

---
+++ Assets/Script/GameScene/GameTime.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+
+public class GameTime : MonoBehaviour
+{
+    [SerializeField] Text text;
+    int time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PhotonNetwork.IsMasterClient)
+            CustomProperties.SetProperties(PhotonNetwork.MasterClient, "gametime", 600);
+
+        time = (int)CustomProperties.GetProperties(PhotonNetwork.MasterClient, "gametime", 600);
+
+        StartCoroutine(ShowTime());
+    }
+
+    IEnumerator ShowTime()
+    {
+        while (time >= 0)
+        {
+            int minutes = time / 60;
+            int seconds = time % 60;
+
+            string secondsStr;
+            if (seconds < 10)
+                secondsStr = "0" + seconds;
+            else
+                secondsStr = seconds.ToString();
+
+            text.text = string.Format("{0}:{1}", minutes, secondsStr);
+            yield return new WaitForSeconds(1.0f);
+            time--;
+        }
+    }
+}
