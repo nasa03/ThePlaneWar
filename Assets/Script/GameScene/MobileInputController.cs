a8f80e2f8c1f13738e71ba2fc4a474fb28506413
SmallPig1997 <664235822@qq.com>
2019-11-22 16:45:35 +0800

    终于解决一直以来的多人模式下摇杆无法移动的bug

--- Assets/Script/GameScene/MobileInputController.cs
+++ Assets/Script/GameScene/MobileInputController.cs
@@ -1,8 +1,7 @@
 ﻿using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityStandardAssets.CrossPlatformInput;
-[RequireComponent(typeof(UnityEngine.UI.AspectRatioFitter))]
-public class MobileInputController : MonoBehaviour,IBeginDragHandler,IDragHandler,IEndDragHandler,IPointerDownHandler,IPointerUpHandler {
+
+public class MobileInputController : MonoBehaviour {
 
     public RectTransform Background;
     public RectTransform Knob;
@@ -10,53 +9,42 @@ public class MobileInputController : MonoBehaviour,IBeginDragHandler,IDragHandle
     public float Horizontal = 0;
     public float Vertical = 0;
 
-
     public float offset;
     Vector2 PointPosition;
 
-    public void OnBeginDrag(PointerEventData eventData)
-    {
-        
-    }
+    // Update is called once per frame
+    void Update () {
 
-    public void OnDrag(PointerEventData eventData)
-    {
-       
-        PointPosition = new Vector2((eventData.position.x - Background.position.x) / ((Background.rect.size.x - Knob.rect.size.x) / 2), (eventData.position.y - Background.position.y) / ((Background.rect.size.y - Knob.rect.size.y) / 2));
-        
-        PointPosition = (PointPosition.magnitude>1.0f)?PointPosition.normalized :PointPosition;
-     
-        Knob.transform.position = new Vector2((PointPosition.x *((Background.rect.size.x-Knob.rect.size.x)/2)*offset) + Background.position.x, (PointPosition.y* ((Background.rect.size.y-Knob.rect.size.y)/2) *offset) + Background.position.y);
-       
-        
-    }
+        if (Input.touchCount != 1)
+            return;
 
-    public void OnEndDrag(PointerEventData eventData)
-    {
-        PointPosition = new Vector2(0f,0f);
-        Knob.transform.position = Background.position;
-    }
-    public void OnPointerDown(PointerEventData eventData)
-    {
-        OnDrag(eventData);
-       
-    }
+        Touch[] touches = Input.touches;
+        foreach (Touch touch in touches)
+        {
+            if (touch.phase != TouchPhase.Canceled && touch.phase != TouchPhase.Ended)
+            {
+                Vector2 position = touch.position;
 
-    public void OnPointerUp(PointerEventData eventData) {
-        OnEndDrag(eventData);
-    }
-   
-	
-	// Update is called once per frame
-	void Update () {
+                if (position.x > Background.position.x - (Background.rect.width / 2) && position.x < Background.position.x + (Background.rect.width / 2)
+                && position.y > Background.position.y - (Background.rect.height / 2) && position.y < Background.position.y + (Background.rect.height / 2))
+                {
+                    PointPosition = new Vector2((position.x - Background.position.x) / ((Background.rect.size.x - Knob.rect.size.x) / 2), (position.y - Background.position.y) / ((Background.rect.size.y - Knob.rect.size.y) / 2));
+                    PointPosition = (PointPosition.magnitude > 1.0f) ? PointPosition.normalized : PointPosition;
+                    Knob.transform.position = new Vector2((PointPosition.x * ((Background.rect.size.x - Knob.rect.size.x) / 2) * offset) + Background.position.x, (PointPosition.y * ((Background.rect.size.y - Knob.rect.size.y) / 2) * offset) + Background.position.y);
+                }
+            }
+            else
+            {
+                PointPosition = new Vector2(0f, 0f);
+                Knob.transform.position = Background.position;
+            }
+        }
+
+
         Horizontal = PointPosition.x;
         Vertical = PointPosition.y;
+
         CrossPlatformInputManager.SetAxis("Horizontal", Horizontal);
         CrossPlatformInputManager.SetAxis("Vertical", Vertical);
-    }
-
-    public Vector2 Coordinate()
-    {
-        return new Vector2(Horizontal,Vertical);
     }
 }
