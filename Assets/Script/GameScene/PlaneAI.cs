02e8619703fcdbaa44728500dd1ccd4e05ad757d
SmallPig1997 <664235822@qq.com>
2020-05-13 20:44:05 +0800

    机器人飞机可加载到场景中

--- Assets/Script/GameScene/PlaneAI.cs
+++ Assets/Script/GameScene/PlaneAI.cs
@@ -6,20 +6,22 @@ using Photon.Pun;
 public class PlaneAI : MonoBehaviour
 {
     [SerializeField] GameObject[] AI_Plane_Prefabs;
-    private ArrayList AI_Plane_List;
+    private ArrayList AI_Plane_List = new ArrayList();
     
     // Start is called before the first frame update
     void Start()
     {
         if (PhotonNetwork.LocalPlayer.IsMasterClient)
         {
-            int[] AI_Plane_Index = (int[]) CustomProperties.GetProperties(PhotonNetwork.CurrentRoom, "AI_List");
-            int playerCount = PhotonNetwork.CurrentRoom.PlayerCount;
-            for (int i = 0; i < 6; i++)
+            object[] AI_Plane_Index = (object[]) CustomProperties.GetProperties(PhotonNetwork.CurrentRoom, "AI_List");
+            int playerCount = PhotonNetwork.CurrentRoom.Players.Count;
+            for (int i = 0; i < AI_Plane_Index.Length; i++)
             {
-                GameObject AI_Plane = PhotonNetwork.Instantiate(AI_Plane_Prefabs[AI_Plane_Index[i]].name,
-                    FindObjectOfType<PhotonGame>().GroundRunwayPosotion[playerCount + i].position, Quaternion.identity);
-                AI_Plane_List.Add(AI_Plane_List);
+                GameObject AI_Plane = PhotonNetwork.Instantiate(
+                    AI_Plane_Prefabs[(int) AI_Plane_Index[i]].name,
+                    FindObjectOfType<PhotonGame>().GroundRunwayPosotion[playerCount + i].position +
+                    new Vector3(0, 15, 0), Quaternion.identity);
+                AI_Plane_List.Add(AI_Plane);
             }
         }
     }
