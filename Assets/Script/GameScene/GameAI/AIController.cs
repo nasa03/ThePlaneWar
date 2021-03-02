63fe30774cc9c16592becc00b855822163907a2b
SmallPig1997 <664235822@qq.com>
2021-03-02 20:43:39 +0800

    AI前方1000米有敌机时会追踪敌机

--- Assets/Script/GameScene/GameAI/AIController.cs
+++ Assets/Script/GameScene/GameAI/AIController.cs
@@ -20,11 +20,13 @@ public class AIController : MonoBehaviour
 
         if (Target) return;
 
-        Transform tempTarget = GetNearTargetTransform();
-        if (!tempTarget)
-            tempTarget = GetRandomPosition();
-
-        Target = tempTarget;
+        Transform tempTarget = Global.GetNearTargetTransform(transform);
+        
+        if (tempTarget && GetDistance(tempTarget) < 1000.0f)
+            Target = tempTarget;
+        else
+            Target = GetRandomPosition();
+        
         _aeroplaneAiControl.SetTarget(Target);
     }
 
@@ -33,55 +35,31 @@ public class AIController : MonoBehaviour
         _aeroplaneAiControl = GetComponent<AeroplaneAiControl>();
     }
 
-    private Transform GetNearTargetTransform()
-    {
-        ArrayList list = new ArrayList();
-
-        GameObject.FindGameObjectsWithTag("Plane").ToList().ForEach(target => list.Add(target.transform));
-
-        GameObject.FindGameObjectsWithTag("AI").ToList().ForEach(delegate(GameObject aiTarget)
-        {
-            if (aiTarget != gameObject)
-                list.Add(aiTarget.transform);
-        });
-
-        Vector3 thisPosition = transform.position;
-        float[] distances = new float[list.Count];
-        for (int i = 0; i < list.Count; i++)
-        {
-            Vector3 targetPosition = (list[i] as Transform).position;
-            Vector3 dir = targetPosition - thisPosition;
-            float dot = Vector3.Dot(Vector3.forward, dir);
-            if (dot > 0)
-                distances[i] = Vector3.Distance(thisPosition, targetPosition);
-            else
-                distances[i] = 0;
-        }
-
-        float minDistance = 0;
-        Transform minTarget = null;
-        for (int i = 0; i < distances.Length; i++)
-        {
-            if (distances[i] == 0 || (minDistance != 0 && !(distances[i] < minDistance))) continue;
-
-            minDistance = distances[i];
-            minTarget = list[i] as Transform;
-        }
-
-        if (minDistance <= 500.0f && minTarget)
-            return minTarget;
-
-        return null;
-    }
-
     private Transform GetRandomPosition()
     {
         int random = -1;
-        
+
         do random = Random.Range(0, FindObjectOfType<PhotonGameAI>().RandomPositions.Length);
         while (random == _random);
 
         _random = random;
         return FindObjectOfType<PhotonGameAI>().RandomPositions[random];
+    }
+
+    private float GetDistance(Transform target)
+    {
+        float distance = 0;
+        
+        if (Target.CompareTag("Plane") && Target.CompareTag("AI"))
+        {
+            Vector3 thisPosition = transform.position;
+            Vector3 targetPosition = target.position;
+            Vector3 dir = targetPosition - thisPosition;
+            float dot = Vector3.Dot(Vector3.forward, dir);
+            if (dot > 0)
+                distance = Vector3.Distance(thisPosition, targetPosition);
+        }
+
+        return distance;
     }
 }
